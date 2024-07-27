8f6f3b8b13713a150a527a42b3a5229283090f04
jwilliams132 <jwilliams1399@gmail.com>
2024-07-26 21:39:29 -0500

    added jackson dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,10 @@
 module jwilliams132 {
-    requires javafx.controls;
-    requires javafx.fxml;
 
-    opens jwilliams132 to javafx.fxml;
-    exports jwilliams132;
+	requires transitive javafx.controls;
+	requires transitive javafx.fxml;
+	requires com.fasterxml.jackson.databind;
+
+	opens jwilliams132 to javafx.fxml;
+
+	exports jwilliams132;
 }
