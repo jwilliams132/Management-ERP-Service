3d59834b1b0a5428fcb3abc553b4357903a06b93
jwilliams132 <jwilliams1399@gmail.com>
2024-08-10 23:41:05 -0500

    made some comment notes

--- src/main/java/jwilliams132/Controller_Reports.java
+++ src/main/java/jwilliams132/Controller_Reports.java
@@ -1,5 +1,14 @@
 package jwilliams132;
 
 public class Controller_Reports {
-	
+	/**
+	 * types of reports:
+	 * Customer based history - list of all sales to specific customer
+	 * 
+	 * Current Pricing Sheet - like a pricing menu to print and give to customers
+	 * - - (learn to use dependency for Word doc manipulation)
+	 * 
+	 * 
+	 */
+
 }
