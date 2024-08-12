086e64373097d26a1c5d0848b4fe288a86ffbbf9
jwilliams132 <jwilliams1399@gmail.com>
2024-08-11 21:32:03 -0500

    more notes

--- src/main/java/jwilliams132/Controller_Reports.java
+++ src/main/java/jwilliams132/Controller_Reports.java
@@ -11,11 +11,14 @@ public class Controller_Reports {
 	 * Current Pricing Sheet - like a pricing menu to print and give to customers
 	 * - - (learn to use dependency for Word doc manipulation)
 	 * 
+	 * purchase form - like a receipt for the customers to sign when they get the
+	 * tires (singe Transaction ID)
+	 * 
 	 * 
 	 */
 
 	public void setup() {
-		
+
 	}
 
 	public void setStorageManager(Stored_Files_Manager storageManager) {
