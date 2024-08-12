92fb4b0a010be2a54c06ddfb1c450eaed23613bd
jwilliams132 <jwilliams1399@gmail.com>
2024-08-11 21:28:58 -0500

    renaming, cleanup, and organization

--- src/main/java/jwilliams132/Controller_Sales.java
+++ src/main/java/jwilliams132/Controller_Sales.java
@@ -31,12 +31,12 @@ import javafx.util.converter.LocalDateTimeStringConverter;
 
 public class Controller_Sales {
 
-	private Stored_Files_Manager storageManager;
-
 	private enum Validity {
 
 		VALID, INVALID, NOT_FOUND, DEFAULT;
 	}
+
+	private Stored_Files_Manager storageManager;
 
 	@FXML
 	private BorderPane salesRoot;
@@ -54,27 +54,22 @@ public class Controller_Sales {
 	private TableColumn<Sale, Integer> quantitySoldColumn;
 	@FXML
 	private TableColumn<Sale, Price_Category> priceCategoryColumn;
-
 	@FXML
 	private TableColumn<Sale, BigDecimal> totalPriceColumn;
+
 	@FXML
 	private TabPane tabPane;
 	@FXML
 	private Label newOrderTransactionID;
 	@FXML
-	private ComboBox<String> tireComboBox_New, tireComboBox_Edit;
+	private TextField transactionIDTextField;
 	@FXML
 	private ComboBox<String> customerComboBox_New, customerComboBox_Edit;
 	@FXML
-	private TextField quantityTextField_New, quantityTextField_Edit, transactionIDTextField_Edit;
-	@FXML
-	private ComboBox<Price_Category> priceCategoryComboBox_New, priceCategoryComboBox_Edit;
-	@FXML
 	private VBox tiresFieldsBox_New, tiresFieldsBox_Edit;
-	@FXML
 	private Button newTireTypeButton_New, newTireTypeButton_Edit;
-	private List<TireInputGroup_Sales> tireInputGroups_New = new ArrayList<TireInputGroup_Sales>();
 
+	private List<TireInputGroup_Sales> tireInputGroups_New = new ArrayList<TireInputGroup_Sales>();
 	private List<TireInputGroup_Sales> tireInputGroups_Edit = new ArrayList<TireInputGroup_Sales>();
 
 	public void setup() {
@@ -90,7 +85,7 @@ public class Controller_Sales {
 		populateCustomerComboBox(customerComboBox_Edit);
 
 		addRightClickFunctionToSelectRowForEdit();
-		addTextFieldListener(transactionIDTextField_Edit);
+		addTextFieldListener(transactionIDTextField);
 		addTabPaneListener();
 
 		initializeNewTireButtons();
@@ -140,20 +135,16 @@ public class Controller_Sales {
 		handleClearFields_New();
 	}
 
-	// ====================================================================================================
-	// handleChangeOrder()
-	// ====================================================================================================
-
 	@FXML
 	private void handleChangeOrder() {
 
-		if (!transactionIDTextField_Edit.getStyleClass().contains("valid")) {
+		if (!transactionIDTextField.getStyleClass().contains("valid")) {
 
 			System.err.println("Transaction ID is not Valid."); // TODO make into an alert popup
 			return;
 		}
 
-		int transactionID = Integer.parseInt(transactionIDTextField_Edit.getText());
+		int transactionID = Integer.parseInt(transactionIDTextField.getText());
 		String chosenCustomer = customerComboBox_Edit.getSelectionModel().getSelectedItem();
 
 		List<Sale> sameIDSaleList = storageManager.getSaleHistory().stream()
@@ -178,7 +169,8 @@ public class Controller_Sales {
 		storageManager.sortTransactionListByID();
 	}
 
-	private void removeExtraStorageEntries(List<Sale> sameIDSaleList, Set<String> newTiresList,
+	private void removeExtraStorageEntries(List<Sale> sameIDSaleList,
+			Set<String> newTiresList,
 			Set<String> oldTiresList) {
 
 		for (String oldTire : oldTiresList) {
@@ -287,8 +279,6 @@ public class Controller_Sales {
 	}
 
 	// ====================================================================================================
-	// End handleChangeOrder
-	// ====================================================================================================
 
 	@FXML
 	private void handleClearFields_New() {
@@ -302,7 +292,7 @@ public class Controller_Sales {
 	@FXML
 	private void handleClearFields_Edit() {
 
-		transactionIDTextField_Edit.clear();
+		transactionIDTextField.clear();
 		customerComboBox_Edit.getSelectionModel().clearSelection();
 		tiresFieldsBox_Edit.getChildren().clear();
 		addTireFieldsGroupToDisplayAndList(tireInputGroups_New, tiresFieldsBox_Edit);
@@ -320,91 +310,77 @@ public class Controller_Sales {
 		populateCustomerComboBox(customerComboBox_New);
 	}
 
-	@FXML
-	private void handleNewTireTypeButton_New() {
+	// ====================================================================================================
+	
+	private void bindColumnsToFields() {
 
-		tiresFieldsBox_New.getChildren().remove(newTireTypeButton_New);
-		addTireFieldsGroupToDisplayAndList(tireInputGroups_New, tiresFieldsBox_New);
-		tiresFieldsBox_New.getChildren().add(newTireTypeButton_New);
-	}
+		timeColumn.setCellValueFactory(new PropertyValueFactory<>("time"));
+		timeColumn.setCellFactory(column -> new TableCell<Sale, LocalDateTime>() {
 
-	@FXML
-	private void handleNewTireTypeButton_Edit() {
+			private final LocalDateTimeStringConverter converter = new LocalDateTimeStringConverter();
 
-		tiresFieldsBox_Edit.getChildren().remove(newTireTypeButton_Edit);
-		addTireFieldsGroupToDisplayAndList(tireInputGroups_Edit, tiresFieldsBox_Edit);
-		tiresFieldsBox_Edit.getChildren().add(newTireTypeButton_Edit);
-	}
+			@Override
+			protected void updateItem(LocalDateTime item, boolean empty) {
 
-	private void setEditOrderFields(Sale chosenTransactionItem) {
+				super.updateItem(item, empty);
+				if (empty)
+					setText(null);
+				else
+					setText(converter.toString(item));
 
-		transactionIDTextField_Edit.setText(String.valueOf(chosenTransactionItem.getTransactionID()));
-		customerComboBox_Edit.getSelectionModel().select(chosenTransactionItem.getCustomer().getName());
-
-		List<Sale> transactionsToEdit = storageManager.getSaleHistory().stream()
-				.filter(sale -> sale.getTransactionID() == chosenTransactionItem.getTransactionID())
-				.collect(Collectors.toList());
-
-		List<TireInputGroup_Sales> controlGroups = new ArrayList<TireInputGroup_Sales>();
-		List<String> skuNumbers = storageManager.getTireInventory()
-				.stream()
-				.map(Tire::getSkuNumber)
-				.collect(Collectors.toList());
-		for (Sale transaction : transactionsToEdit) {
-
-			controlGroups.add(new TireInputGroup_Sales(skuNumbers, transaction.getSkuNumber(),
-					transaction.getQuantity(), transaction.getPriceCategory()));
-		}
-		tireInputGroups_Edit = controlGroups;
-		addRightClickFunctionToDeleteControlGroup(tireInputGroups_Edit);
-		updateTireControlGroupsOnEditDisplay();
-	}
-
-	private void addTabPaneListener() {
-
-		tabPane.getSelectionModel().selectedItemProperty().addListener((observable, oldTab, newTab) -> {
-
-			if (newTab != null)
-				setNewOrderTransactionIDLabel();
-
-			if (newTab == tabPane.getTabs().get(0)) {
-
-				tabPane.getStyleClass().remove("edit-tab-open");
-				tabPane.getStyleClass().add("new-tab-open");
 			}
-			if (newTab == tabPane.getTabs().get(1)) {
+		});
 
-				tabPane.getStyleClass().remove("new-tab-open");
-				tabPane.getStyleClass().add("edit-tab-open");
+		transactionIdColumn.setCellValueFactory(new PropertyValueFactory<>("transactionID"));
+		skuNumberColumn.setCellValueFactory(new PropertyValueFactory<>("skuNumber"));
+		quantitySoldColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+		customerNameColumn.setCellValueFactory(cellData -> cellData.getValue().getCustomer().nameProperty());
+		priceCategoryColumn.setCellValueFactory(new PropertyValueFactory<>("priceCategory"));
+		totalPriceColumn.setCellValueFactory(new PropertyValueFactory<>("totalPrice"));
+	}
+
+	// ====================================================================================================
+
+	private <S, T> void setRightAlignment(TableColumn<S, T> column) {
+
+		column.setCellFactory(new Callback<TableColumn<S, T>, TableCell<S, T>>() {
+
+			@Override
+			public TableCell<S, T> call(TableColumn<S, T> param) {
+
+				return new TableCell<S, T>() {
+
+					@Override
+					protected void updateItem(T item, boolean empty) {
+
+						super.updateItem(item, empty);
+						if (empty || item == null) {
+
+							setText(null);
+						} else {
+
+							setText(item.toString());
+						}
+						setAlignment(Pos.CENTER_RIGHT);
+					}
+				};
 			}
 		});
 	}
 
-	private void addTireFieldsGroupToDisplayAndList(List<TireInputGroup_Sales> groupList, VBox vbox) {
+	// ====================================================================================================
 
-		TireInputGroup_Sales controlGroup = new TireInputGroup_Sales(storageManager.getTireInventory()
-				.stream()
-				.map(Tire::getSkuNumber)
-				.collect(Collectors.toList()));
-		groupList.add(controlGroup);
-		vbox.getChildren().add(controlGroup.getControlGroupContainer());
-		addRightClickFunctionToDeleteControlGroup(groupList);
+	private void populateCustomerComboBox(ComboBox<String> customerComboBox) {
+
+		customerComboBox.setItems(
+				FXCollections.observableArrayList(
+						storageManager.getCustomerList()
+								.stream()
+								.map(Customer::getName)
+								.collect(Collectors.toList())));
 	}
 
-	private void initializeNewTireButtons() {
-
-		newTireTypeButton_New = new Button("+ Tire Type");
-		newTireTypeButton_New.setOnAction(event -> {
-
-			handleNewTireTypeButton_New();
-		});
-
-		newTireTypeButton_Edit = new Button("+ Tire Type");
-		newTireTypeButton_Edit.setOnAction(event -> {
-
-			handleNewTireTypeButton_Edit();
-		});
-	}
+	// ====================================================================================================
 
 	private void addRightClickFunctionToSelectRowForEdit() {
 
@@ -431,54 +407,49 @@ public class Controller_Sales {
 		});
 	}
 
-	private void addRightClickFunctionToDeleteControlGroup(List<TireInputGroup_Sales> groups) {
+	private void setEditOrderFields(Sale chosenTransactionItem) {
 
-		for (TireInputGroup_Sales group : groups) {
+		transactionIDTextField.setText(String.valueOf(chosenTransactionItem.getTransactionID()));
+		customerComboBox_Edit.getSelectionModel().select(chosenTransactionItem.getCustomer().getName());
 
-			ContextMenu contextMenu = new ContextMenu();
-			MenuItem deleteItem = new MenuItem("Delete");
+		List<Sale> transactionsToEdit = storageManager.getSaleHistory().stream()
+				.filter(sale -> sale.getTransactionID() == chosenTransactionItem.getTransactionID())
+				.collect(Collectors.toList());
 
-			contextMenu.getItems().add(deleteItem);
-			VBox vbox = group.getControlGroupContainer(); // Assuming you have a method to get the VBox
-			vbox.setOnContextMenuRequested(event -> contextMenu.show(vbox, event.getScreenX(), event.getScreenY()));
+		List<TireInputGroup_Sales> controlGroups = new ArrayList<TireInputGroup_Sales>();
+		List<String> skuNumbers = storageManager.getTireInventory()
+				.stream()
+				.map(Tire::getSkuNumber)
+				.collect(Collectors.toList());
+		for (Sale transaction : transactionsToEdit) {
 
-			// Handle delete action
-			deleteItem.setOnAction(e -> {
-
-				// Remove the VBox from the UI
-				((Pane) vbox.getParent()).getChildren().remove(vbox);
-
-				// Optionally remove it from the editTireFieldsList if needed
-				groups.remove(group);
-			});
+			controlGroups.add(new TireInputGroup_Sales(skuNumbers, transaction.getSkuNumber(),
+					transaction.getQuantity(), transaction.getPriceCategory()));
 		}
+		tireInputGroups_Edit = controlGroups;
+		addRightClickFunctionToDeleteControlGroup(tireInputGroups_Edit);
+		updateInputGroupsOnEditDisplay();
 	}
 
-	private void bindColumnsToFields() {
+	private void updateInputGroupsOnEditDisplay() {
 
-		timeColumn.setCellValueFactory(new PropertyValueFactory<>("time"));
-		timeColumn.setCellFactory(column -> new TableCell<Sale, LocalDateTime>() {
+		tiresFieldsBox_Edit.getChildren().clear();
+		for (TireInputGroup_Sales controlGroup : tireInputGroups_Edit) {
 
-			private final LocalDateTimeStringConverter converter = new LocalDateTimeStringConverter();
+			tiresFieldsBox_Edit.getChildren().add(controlGroup.getControlGroupContainer());
+		}
+		tiresFieldsBox_Edit.getChildren().add(newTireTypeButton_Edit);
+		addRightClickFunctionToDeleteControlGroup(tireInputGroups_Edit);
+	}
 
-			@Override
-			protected void updateItem(LocalDateTime item, boolean empty) {
+	// ====================================================================================================
 
-				super.updateItem(item, empty);
-				if (empty)
-					setText(null);
-				else
-					setText(converter.toString(item));
+	private void addTextFieldListener(TextField textField) {
 
-			}
+		textField.textProperty().addListener((observable, oldValue, newValue) -> {
+
+			handleTransactionIDTextChanged(textField, oldValue, newValue);
 		});
-
-		transactionIdColumn.setCellValueFactory(new PropertyValueFactory<>("transactionID"));
-		skuNumberColumn.setCellValueFactory(new PropertyValueFactory<>("skuNumber"));
-		quantitySoldColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
-		customerNameColumn.setCellValueFactory(cellData -> cellData.getValue().getCustomer().nameProperty());
-		priceCategoryColumn.setCellValueFactory(new PropertyValueFactory<>("priceCategory"));
-		totalPriceColumn.setCellValueFactory(new PropertyValueFactory<>("totalPrice"));
 	}
 
 	private void handleTransactionIDTextChanged(TextField textField, String oldValue, String newValue) {
@@ -515,36 +486,6 @@ public class Controller_Sales {
 		setEditOrderFields(attemptedSale);
 	}
 
-	// ===========================================================================
-	// Helper methods
-	// ===========================================================================
-
-	private void setNewOrderTransactionIDLabel() {
-
-		newOrderTransactionID.setText(String.valueOf(storageManager.getNewTransactionID()));
-	}
-
-	private void updateTireControlGroupsOnEditDisplay() {
-
-		tiresFieldsBox_Edit.getChildren().clear();
-		for (TireInputGroup_Sales controlGroup : tireInputGroups_Edit) {
-
-			tiresFieldsBox_Edit.getChildren().add(controlGroup.getControlGroupContainer());
-		}
-		tiresFieldsBox_Edit.getChildren().add(newTireTypeButton_Edit);
-		addRightClickFunctionToDeleteControlGroup(tireInputGroups_Edit);
-	}
-
-	private void populateCustomerComboBox(ComboBox<String> customerComboBox) {
-
-		customerComboBox.setItems(
-				FXCollections.observableArrayList(
-						storageManager.getCustomerList()
-								.stream()
-								.map(Customer::getName)
-								.collect(Collectors.toList())));
-	}
-
 	private void setInputTextValidityColor(TextField textField, Validity validity) {
 
 		textField.getStyleClass().removeAll("valid", "invalid", "not-found");
@@ -567,40 +508,99 @@ public class Controller_Sales {
 		}
 	}
 
-	private void addTextFieldListener(TextField textField) {
+	// ====================================================================================================
 
-		textField.textProperty().addListener((observable, oldValue, newValue) -> {
+	private void addTabPaneListener() {
 
-			handleTransactionIDTextChanged(textField, oldValue, newValue);
-		});
-	}
+		tabPane.getSelectionModel().selectedItemProperty().addListener((observable, oldTab, newTab) -> {
 
-	private <S, T> void setRightAlignment(TableColumn<S, T> column) {
+			if (newTab != null)
+				setNewOrderTransactionIDLabel();
 
-		column.setCellFactory(new Callback<TableColumn<S, T>, TableCell<S, T>>() {
+			if (newTab == tabPane.getTabs().get(0)) {
 
-			@Override
-			public TableCell<S, T> call(TableColumn<S, T> param) {
+				tabPane.getStyleClass().remove("edit-tab-open");
+				tabPane.getStyleClass().add("new-tab-open");
+			}
+			if (newTab == tabPane.getTabs().get(1)) {
 
-				return new TableCell<S, T>() {
-
-					@Override
-					protected void updateItem(T item, boolean empty) {
-
-						super.updateItem(item, empty);
-						if (empty || item == null) {
-
-							setText(null);
-						} else {
-
-							setText(item.toString());
-						}
-						setAlignment(Pos.CENTER_RIGHT);
-					}
-				};
+				tabPane.getStyleClass().remove("new-tab-open");
+				tabPane.getStyleClass().add("edit-tab-open");
 			}
 		});
 	}
+
+	private void setNewOrderTransactionIDLabel() {
+
+		newOrderTransactionID.setText(String.valueOf(storageManager.getNewTransactionID()));
+	}
+
+	// ====================================================================================================
+
+	private void initializeNewTireButtons() {
+
+		newTireTypeButton_New = new Button("+ Tire Type");
+		newTireTypeButton_New.setOnAction(event -> {
+
+			handleNewTireTypeButton_New();
+		});
+
+		newTireTypeButton_Edit = new Button("+ Tire Type");
+		newTireTypeButton_Edit.setOnAction(event -> {
+
+			handleNewTireTypeButton_Edit();
+		});
+	}
+
+	private void handleNewTireTypeButton_New() {
+
+		tiresFieldsBox_New.getChildren().remove(newTireTypeButton_New);
+		addTireFieldsGroupToDisplayAndList(tireInputGroups_New, tiresFieldsBox_New);
+		tiresFieldsBox_New.getChildren().add(newTireTypeButton_New);
+	}
+
+	private void handleNewTireTypeButton_Edit() {
+
+		tiresFieldsBox_Edit.getChildren().remove(newTireTypeButton_Edit);
+		addTireFieldsGroupToDisplayAndList(tireInputGroups_Edit, tiresFieldsBox_Edit);
+		tiresFieldsBox_Edit.getChildren().add(newTireTypeButton_Edit);
+	}
+
+	private void addTireFieldsGroupToDisplayAndList(List<TireInputGroup_Sales> groupList, VBox vbox) {
+
+		TireInputGroup_Sales controlGroup = new TireInputGroup_Sales(storageManager.getTireInventory()
+				.stream()
+				.map(Tire::getSkuNumber)
+				.collect(Collectors.toList()));
+		groupList.add(controlGroup);
+		vbox.getChildren().add(controlGroup.getControlGroupContainer());
+		addRightClickFunctionToDeleteControlGroup(groupList);
+	}
+
+	private void addRightClickFunctionToDeleteControlGroup(List<TireInputGroup_Sales> groups) {
+
+		for (TireInputGroup_Sales group : groups) {
+
+			ContextMenu contextMenu = new ContextMenu();
+			MenuItem deleteItem = new MenuItem("Delete");
+
+			contextMenu.getItems().add(deleteItem);
+			VBox vbox = group.getControlGroupContainer(); // Assuming you have a method to get the VBox
+			vbox.setOnContextMenuRequested(event -> contextMenu.show(vbox, event.getScreenX(), event.getScreenY()));
+
+			// Handle delete action
+			deleteItem.setOnAction(e -> {
+
+				// Remove the VBox from the UI
+				((Pane) vbox.getParent()).getChildren().remove(vbox);
+
+				// Optionally remove it from the editTireFieldsList if needed
+				groups.remove(group);
+			});
+		}
+	}
+
+	// ====================================================================================================
 
 	public void setStorageManager(Stored_Files_Manager storageManager) {
 
