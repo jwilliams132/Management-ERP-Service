f5fedb8afb90b6cb1ff9606f79123792d5ba22fb
jwilliams132 <jwilliams1399@gmail.com>
2024-08-16 16:10:00 -0500

    added ability to show CSS changes live

--- src/main/java/jwilliams132/App.java
+++ src/main/java/jwilliams132/App.java
@@ -2,8 +2,16 @@ package jwilliams132;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -57,6 +65,8 @@ public class App extends Application {
 		setWindowIcon();
 		window.setOnCloseRequest(e -> window.close());
 		window.show();
+
+		startCssFileWatcher();
 	}
 
 	private void setWindowIcon() {
@@ -69,5 +79,49 @@ public class App extends Application {
 
 			System.err.println("Failed to load the image stream");
 		}
+	}
+
+	private void reloadStylesheets() {
+		root.getStylesheets().clear();
+		root.getStylesheets().add(CSS_Styles);
+		root.getStylesheets().add(CSS_Colors_Dark);
+	}
+
+	private void startCssFileWatcher() {
+
+		Path pathStyles = Paths.get("src\\main\\resources\\jwilliams132\\Element_Styles.css").toAbsolutePath();
+		Path pathColors = Paths.get("src\\main\\resources\\jwilliams132\\Element_Colors_Dark.css").toAbsolutePath();
+
+		Runnable cssWatcherTask = () -> {
+
+			try {
+
+				WatchService watchService = FileSystems.getDefault().newWatchService();
+				pathStyles.getParent().register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
+				pathColors.getParent().register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
+
+				while (true) {
+
+					WatchKey key = watchService.take();
+					for (WatchEvent<?> event : key.pollEvents()) {
+
+						if (event.kind() == StandardWatchEventKinds.ENTRY_MODIFY &&
+								(event.context().toString().equals(pathStyles.getFileName().toString()) ||
+										event.context().toString().equals(pathColors.getFileName().toString()))) {
+
+							Platform.runLater(this::reloadStylesheets);
+						}
+					}
+					key.reset();
+				}
+			} catch (Exception e) {
+
+				e.printStackTrace();
+			}
+		};
+
+		Thread cssWatcherThread = new Thread(cssWatcherTask);
+		cssWatcherThread.setDaemon(true);
+		cssWatcherThread.start();
 	}
 }
