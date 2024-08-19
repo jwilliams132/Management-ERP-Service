1a6481f8c2fe6095ac0fcb0bfeefbaac70f5edd1
jwilliams132 <jwilliams1399@gmail.com>
2024-08-19 00:36:17 -0500

    connected stored data to table and chart

--- src/main/java/jwilliams132/Controller_Inventory.java
+++ src/main/java/jwilliams132/Controller_Inventory.java
@@ -1,14 +1,24 @@
 package jwilliams132;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Tooltip;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.util.Callback;
@@ -18,11 +28,13 @@ public class Controller_Inventory {
     private Stored_Files_Manager storageManager;
     @FXML
     private BorderPane inventoryRoot;
-    
+
     @FXML
     private TableView<Tire> tireTableView; // Ensure this is parameterized with Tire
     @FXML
-    private LineChart<Integer, Integer> lineChart;
+    private LineChart<Number, Number> lineChart;
+    @FXML
+    private NumberAxis xAxis, yAxis;
 
     @FXML
     private TableColumn<Tire, String> itemIdColumn;
@@ -34,7 +46,7 @@ public class Controller_Inventory {
     private TableColumn<Tire, Integer> tireCountColumn;
 
     private ObservableList<Tire> tireList = FXCollections.observableArrayList();
-
+    private Map<XYChart.Series<Number, Number>, Integer> seriesMap = new HashMap<XYChart.Series<Number, Number>, Integer>();
 
     public void setup() {
 
@@ -49,7 +61,14 @@ public class Controller_Inventory {
         tireList = storageManager.getTireInventory();
         tireTableView.setItems(tireList);
 
-        populateLineChart(); // sample data
+        // populateLineChart(); // sample data
+        for (Tire tire : tireList) {
+
+            generateInventoryGraph(tire);
+        }
+        for (XYChart.Series<Number, Number> series : lineChart.getData()) {
+            addHoverEffect(lineChart, series);
+        }
     }
 
     private <S, T> void setRightAlignment(TableColumn<S, T> column) {
@@ -82,7 +101,7 @@ public class Controller_Inventory {
     public void populateLineChart() {
 
         // Create Series 1
-        XYChart.Series<Integer, Integer> series1 = new XYChart.Series<>();
+        XYChart.Series<Number, Number> series1 = new XYChart.Series<>();
         series1.setName("Portfolio 1");
         series1.getData().add(new XYChart.Data<>(0, 25));
         series1.getData().add(new XYChart.Data<>(1, 23));
@@ -100,7 +119,7 @@ public class Controller_Inventory {
         series1.getData().add(new XYChart.Data<>(13, 25));
 
         // Create Series 2
-        XYChart.Series<Integer, Integer> series2 = new XYChart.Series<>();
+        XYChart.Series<Number, Number> series2 = new XYChart.Series<>();
         series2.setName("Portfolio 2");
         series2.getData().add(new XYChart.Data<>(1, 12));
         series2.getData().add(new XYChart.Data<>(2, 18));
@@ -116,7 +135,7 @@ public class Controller_Inventory {
         series2.getData().add(new XYChart.Data<>(12, 31));
 
         // Create Series 3
-        XYChart.Series<Integer, Integer> series3 = new XYChart.Series<>();
+        XYChart.Series<Number, Number> series3 = new XYChart.Series<>();
         series3.setName("Portfolio 3");
         series3.getData().add(new XYChart.Data<>(1, 16));
         series3.getData().add(new XYChart.Data<>(2, 22));
@@ -132,7 +151,7 @@ public class Controller_Inventory {
         series3.getData().add(new XYChart.Data<>(12, 28));
 
         // Create Series 4
-        XYChart.Series<Integer, Integer> series4 = new XYChart.Series<>();
+        XYChart.Series<Number, Number> series4 = new XYChart.Series<>();
         series4.setName("Portfolio 4");
         series4.getData().add(new XYChart.Data<>(1, 25));
         series4.getData().add(new XYChart.Data<>(2, 20));
@@ -148,7 +167,7 @@ public class Controller_Inventory {
         series4.getData().add(new XYChart.Data<>(12, 26));
 
         // Create Series 5
-        XYChart.Series<Integer, Integer> series5 = new XYChart.Series<>();
+        XYChart.Series<Number, Number> series5 = new XYChart.Series<>();
         series5.setName("Portfolio 5");
         series5.getData().add(new XYChart.Data<>(1, 18));
         series5.getData().add(new XYChart.Data<>(2, 21));
@@ -164,7 +183,7 @@ public class Controller_Inventory {
         series5.getData().add(new XYChart.Data<>(12, 22));
 
         // Create Series 6
-        XYChart.Series<Integer, Integer> series6 = new XYChart.Series<>();
+        XYChart.Series<Number, Number> series6 = new XYChart.Series<>();
         series6.setName("Portfolio 6");
         series6.getData().add(new XYChart.Data<>(1, 20));
         series6.getData().add(new XYChart.Data<>(2, 25));
@@ -180,7 +199,7 @@ public class Controller_Inventory {
         series6.getData().add(new XYChart.Data<>(12, 26));
 
         // Create Series 7
-        XYChart.Series<Integer, Integer> series7 = new XYChart.Series<>();
+        XYChart.Series<Number, Number> series7 = new XYChart.Series<>();
         series7.setName("Portfolio 7");
         series7.getData().add(new XYChart.Data<>(1, 15));
         series7.getData().add(new XYChart.Data<>(2, 18));
@@ -196,7 +215,7 @@ public class Controller_Inventory {
         series7.getData().add(new XYChart.Data<>(12, 22));
 
         // Create Series 8
-        XYChart.Series<Integer, Integer> series8 = new XYChart.Series<>();
+        XYChart.Series<Number, Number> series8 = new XYChart.Series<>();
         series8.setName("Portfolio 8");
         series8.getData().add(new XYChart.Data<>(1, 22));
         series8.getData().add(new XYChart.Data<>(2, 26));
@@ -212,7 +231,7 @@ public class Controller_Inventory {
         series8.getData().add(new XYChart.Data<>(12, 29));
 
         // Create Series 9
-        XYChart.Series<Integer, Integer> series9 = new XYChart.Series<>();
+        XYChart.Series<Number, Number> series9 = new XYChart.Series<>();
         series9.setName("Portfolio 9");
         series9.getData().add(new XYChart.Data<>(1, 14));
         series9.getData().add(new XYChart.Data<>(2, 19));
@@ -228,7 +247,7 @@ public class Controller_Inventory {
         series9.getData().add(new XYChart.Data<>(12, 23));
 
         // Create Series 10
-        XYChart.Series<Integer, Integer> series10 = new XYChart.Series<>();
+        XYChart.Series<Number, Number> series10 = new XYChart.Series<>();
         series10.setName("Portfolio 10");
         series10.getData().add(new XYChart.Data<>(1, 21));
         series10.getData().add(new XYChart.Data<>(2, 26));
@@ -244,7 +263,7 @@ public class Controller_Inventory {
         series10.getData().add(new XYChart.Data<>(12, 29));
 
         // Create Series 11
-        XYChart.Series<Integer, Integer> series11 = new XYChart.Series<>();
+        XYChart.Series<Number, Number> series11 = new XYChart.Series<>();
         series11.setName("Portfolio 11");
         series11.getData().add(new XYChart.Data<>(1, 17));
         series11.getData().add(new XYChart.Data<>(2, 22));
@@ -260,7 +279,7 @@ public class Controller_Inventory {
         series11.getData().add(new XYChart.Data<>(12, 23));
 
         // Create Series 12
-        XYChart.Series<Integer, Integer> series12 = new XYChart.Series<>();
+        XYChart.Series<Number, Number> series12 = new XYChart.Series<>();
         series12.setName("Portfolio 12");
         series12.getData().add(new XYChart.Data<>(1, 26));
         series12.getData().add(new XYChart.Data<>(2, 30));
@@ -276,7 +295,7 @@ public class Controller_Inventory {
         series12.getData().add(new XYChart.Data<>(12, 29));
 
         // Create Series 13
-        XYChart.Series<Integer, Integer> series13 = new XYChart.Series<>();
+        XYChart.Series<Number, Number> series13 = new XYChart.Series<>();
         series13.setName("Portfolio 13");
         series13.getData().add(new XYChart.Data<>(1, 19));
         series13.getData().add(new XYChart.Data<>(2, 24));
@@ -292,7 +311,7 @@ public class Controller_Inventory {
         series13.getData().add(new XYChart.Data<>(12, 25));
 
         // Create Series 14
-        XYChart.Series<Integer, Integer> series14 = new XYChart.Series<>();
+        XYChart.Series<Number, Number> series14 = new XYChart.Series<>();
         series14.setName("Portfolio 14");
         series14.getData().add(new XYChart.Data<>(1, 22));
         series14.getData().add(new XYChart.Data<>(2, 28));
@@ -308,7 +327,7 @@ public class Controller_Inventory {
         series14.getData().add(new XYChart.Data<>(12, 26));
 
         // Create Series 15
-        XYChart.Series<Integer, Integer> series15 = new XYChart.Series<>();
+        XYChart.Series<Number, Number> series15 = new XYChart.Series<>();
         series15.setName("Portfolio 15");
         series15.getData().add(new XYChart.Data<>(1, 27));
         series15.getData().add(new XYChart.Data<>(2, 33));
@@ -324,7 +343,7 @@ public class Controller_Inventory {
         series15.getData().add(new XYChart.Data<>(12, 29));
 
         // Create Series 16
-        XYChart.Series<Integer, Integer> series16 = new XYChart.Series<>();
+        XYChart.Series<Number, Number> series16 = new XYChart.Series<>();
         series16.setName("Portfolio 16");
         series16.getData().add(new XYChart.Data<>(1, 23));
         series16.getData().add(new XYChart.Data<>(2, 28));
@@ -340,7 +359,7 @@ public class Controller_Inventory {
         series16.getData().add(new XYChart.Data<>(12, 28));
 
         // Create Series 17
-        XYChart.Series<Integer, Integer> series17 = new XYChart.Series<>();
+        XYChart.Series<Number, Number> series17 = new XYChart.Series<>();
         series17.setName("Portfolio 17");
         series17.getData().add(new XYChart.Data<>(1, 21));
         series17.getData().add(new XYChart.Data<>(2, 26));
@@ -356,7 +375,7 @@ public class Controller_Inventory {
         series17.getData().add(new XYChart.Data<>(12, 29));
 
         // Create Series 18
-        XYChart.Series<Integer, Integer> series18 = new XYChart.Series<>();
+        XYChart.Series<Number, Number> series18 = new XYChart.Series<>();
         series18.setName("Portfolio 18");
         series18.getData().add(new XYChart.Data<>(1, 18));
         series18.getData().add(new XYChart.Data<>(2, 23));
@@ -372,7 +391,7 @@ public class Controller_Inventory {
         series18.getData().add(new XYChart.Data<>(12, 24));
 
         // Create Series 19
-        XYChart.Series<Integer, Integer> series19 = new XYChart.Series<>();
+        XYChart.Series<Number, Number> series19 = new XYChart.Series<>();
         series19.setName("Portfolio 19");
         series19.getData().add(new XYChart.Data<>(1, 24));
         series19.getData().add(new XYChart.Data<>(2, 29));
@@ -388,7 +407,7 @@ public class Controller_Inventory {
         series19.getData().add(new XYChart.Data<>(12, 25));
 
         // Create Series 20
-        XYChart.Series<Integer, Integer> series20 = new XYChart.Series<>();
+        XYChart.Series<Number, Number> series20 = new XYChart.Series<>();
         series20.setName("Portfolio 20");
         series20.getData().add(new XYChart.Data<>(1, 22));
         series20.getData().add(new XYChart.Data<>(2, 27));
@@ -426,8 +445,155 @@ public class Controller_Inventory {
         lineChart.getData().add(series20);
     }
 
+    public void setupLineChart() {
+
+    }
+
+    public void generateInventoryGraph(Tire tire) {
+
+        final int xAxisRange = 14;
+
+        Map<LocalDate, Integer> inventoryOverTime = new HashMap<>();
+
+        int runningTotal = 0;
+        for (Transaction transaction : storageManager.getTransactionsHistoryOfTire(tire)) {
+
+            if (transaction instanceof Sale)
+                runningTotal -= transaction.getQuantity();
+
+            if (transaction instanceof Purchase)
+                runningTotal += transaction.getQuantity();
+
+            inventoryOverTime.put(transaction.getTime().toLocalDate(), runningTotal);
+        }
+
+        // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+        ObservableList<XYChart.Data<Number, Number>> graphData = FXCollections.observableArrayList();
+        LocalDate today = LocalDate.now();
+
+        // set all points between now and 60 days ago
+        Map.Entry<LocalDate, Integer> seriesStartPoint = null;
+        for (Map.Entry<LocalDate, Integer> entry : inventoryOverTime.entrySet()) {
+
+            if (entry.getKey().isBefore(today.minusDays(xAxisRange))) {
+
+                // System.out.println("bitch");
+                if (seriesStartPoint == null) {
+                    seriesStartPoint = entry;
+                    continue;
+                }
+                seriesStartPoint = entry.getKey().isAfter(seriesStartPoint.getKey()) ? entry : seriesStartPoint;
+                continue;
+            }
+
+            graphData.add(
+                    new XYChart.Data<>(
+                            (ChronoUnit.DAYS.between(entry.getKey(), today)),
+                            entry.getValue()));
+        }
+
+        // Check if there is already a point with the xAxisRange value
+        boolean pointExistsAtXAxisRange = graphData.stream()
+                .anyMatch(data -> data.getXValue().intValue() == xAxisRange);
+
+        if (!pointExistsAtXAxisRange)
+            graphData.add(new XYChart.Data<>(xAxisRange, tire.getInventoryCount())); // add the most current point
+
+        graphData.add(new XYChart.Data<>(0, seriesStartPoint == null ? 0 : seriesStartPoint.getValue())); // set most
+                                                                                                          // current
+                                                                                                          // point
+
+        // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+        XYChart.Series<Number, Number> series = new XYChart.Series<>(graphData);
+        seriesMap.put(series, seriesMap.size());
+        series.setName(tire.getSkuNumber());
+        lineChart.getData().add(series);
+    }
+
+    private void addHoverEffect(LineChart<Number, Number> lineChart, XYChart.Series<Number, Number> series) {
+
+        for (XYChart.Data<Number, Number> data : series.getData()) {
+
+            data.getNode().setOnMouseEntered(event -> {
+
+                System.out.println("Mouse entered: " + data.getXValue()); // Debugging line
+                Tooltip.install(data.getNode(), new Tooltip(String.valueOf(data.getYValue())));
+
+                highlightSeries(lineChart, series);
+            });
+
+            data.getNode().setOnMouseExited(event -> {
+
+                removeHighlightFromSeries(lineChart, series);
+            });
+        }
+
+        // Apply hover effect for legend items
+        System.out.println(lineChart.lookupAll(".chart-legend-item").size());
+        for (Node legendItem : lineChart.lookupAll(".chart-legend-item")) {
+            Label label = (Label) legendItem;
+
+            if (label.getText().equals(series.getName())) {
+
+                System.out.println(label.getText());
+                label.setOnMouseEntered(event -> {
+
+                    System.out.println("Mouse entered on legend: " + series.getName()); // Debugging line
+
+                    highlightSeries(lineChart, series);
+                });
+
+                label.setOnMouseExited(event -> {
+
+                    System.out.println("Mouse exited from legend: " + series.getName()); // Debugging line
+
+                    removeHighlightFromSeries(lineChart, series);
+                });
+            }
+        }
+    }
+
+    private void removeHighlightFromSeries(LineChart<Number, Number> lineChart, XYChart.Series<Number, Number> series) {
+
+        for (Map.Entry<XYChart.Series<Number, Number>, Integer> entry : seriesMap.entrySet()) {
+
+            XYChart.Series<Number, Number> currentSeries = entry.getKey();
+            if (!currentSeries.equals(series)) {
+
+                Set<Node> nodes = lineChart.lookupAll(".series" + seriesMap.get(currentSeries));
+
+                for (Node n : nodes) {
+                    n.getStyleClass().add(String.format("default-color%d",
+                            seriesMap.get(currentSeries) % 8));
+                    n.getStyleClass().remove("greyed-out");
+                }
+            }
+        }
+    }
+
+    private void highlightSeries(LineChart<Number, Number> lineChart, XYChart.Series<Number, Number> series) {
+
+        for (Map.Entry<XYChart.Series<Number, Number>, Integer> entry : seriesMap.entrySet()) {
+
+            XYChart.Series<Number, Number> currentSeries = entry.getKey();
+            if (!currentSeries.equals(series)) {
+
+                Set<Node> nodes = lineChart.lookupAll(".series" + seriesMap.get(currentSeries));
+
+                for (Node n : nodes) {
+
+                    n.getStyleClass().remove(
+                            String.format("default-color%d", seriesMap.get(currentSeries) % 8));
+                    n.getStyleClass().add("greyed-out");
+                }
+            }
+        }
+    }
+
     public void setStorageManager(Stored_Files_Manager storageManager) {
 
-		this.storageManager = storageManager;
-	}
+        this.storageManager = storageManager;
+    }
 }
